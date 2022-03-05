bce6d0a426062b22fe2d824de2def29aed78f053
michaelkcwong <wong_michael@rogers.com>
2022-03-05 00:17:48 -0500

    Displaying appointments, Empty and Show components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react'
 
 import 'components/Appointment/styles.scss';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 export default function Appointment(props) {
-  const formatTime = () => {
-    if(props.time) {
-      return `Appointment at ${props.time}`
-    }
-    if(!props.time) {
-      return "No Appointments"
-    }
-  };
-
   return (
-  <article className="appointment">
-    {formatTime()}
+    <article className='appointment'>
+      <Header time={props.time} />
+      {props.interview ? ( 
+        <Show
+        student={props.interview.student}
+        interviewer={props.interview.interview}
+        />
+      ) : (
+        <Empty />
+      )}
   </article>);
-} 
+};
