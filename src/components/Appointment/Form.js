3836baf57a016b65ead8de0b1bd34c9386d658b7
michaelkcwong <wong_michael@rogers.com>
2022-03-05 00:04:12 -0500

    Add controlled list, props, button actions, resetting inputs, onSubmit handler

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,10 +7,20 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] =useState(props.interviewerm || null);
 
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
-    <form autoComplete="off">
+    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
       <input
         className="appointment__create-input text--semi-bold"
         name="name"
@@ -21,13 +31,15 @@ export default function Form(props) {
       />
     </form>
     <InterviewerList 
-      /* your code goes here */
+      interviewers={props.interviewers}
+      value={interviewer}
+      onChange={setInterviewer}
     />
   </section>
   <section className="appointment__card-right">
     <section className="appointment__actions">
-      <Button danger {/* your code goes here */}>Cancel</Button>
-      <Button confirm {/* your code goes here */}>Save</Button>
+      <Button danger onClick={cancel}>Cancel</Button>
+      <Button confirm onClick={props.onSave}>Save</Button>
     </section>
   </section>
 </main>
