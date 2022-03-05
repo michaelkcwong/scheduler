5ece37a53dfbf4ed164942dacd5f3790147fff1b
michaelkcwong <wong_michael@rogers.com>
2022-03-04 23:49:32 -0500

    Props and state in the appointment form setting a default state, and adding controlled component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
+
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] =useState(props.interviewerm || null);
+
   return (
     <main className="appointment__card appointment__card--create">
   <section className="appointment__card-left">
@@ -12,10 +16,8 @@ export default function Form(props) {
         name="name"
         type="text"
         placeholder="Enter Student Name"
-        /*
-          This must be a controlled component
-          your code goes here
-        */
+        value={student}
+        onChange={(event) => setStudent(event.target.value)}
       />
     </form>
     <InterviewerList 
