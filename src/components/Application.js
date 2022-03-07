09789a2286081e1e028d789b071f900e08e91cd7
michaelkcwong <wong_michael@rogers.com>
2022-03-07 00:41:37 -0500

    Managing state

--- src/components/Application.js
+++ src/components/Application.js
@@ -51,12 +51,16 @@ const appointmentArr = Object.values(appointments).map((obj) => {
 });
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState((prev) => ({ ...prev, days}));
 
   useEffect(() => {
     const url = `http://localhost:8001/api/days`;
-    axios.get(url).then((response) => {
+    axios.get("/api/days").then((response) => {
       setDays(response.data);
     });
   }, []);
@@ -71,7 +75,7 @@ export default function Application(props) {
 />
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
-<DayList days={days} value={day} onChange={setDay} />
+<DayList days={state.days} value={state.day} onChange={setDay} />
 </nav>
 
 <img
