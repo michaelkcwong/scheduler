cac3e0175f51045dd2a47583e0ddfece667ca8b2
michaelkcwong <wong_michael@rogers.com>
2022-03-11 22:29:43 -0500

    Add transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,14 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([]);
 
-  return { mode };
+  function transition(newMode, replace = false) {
+    if (replace === false) {
+      history.push(mode);
+    }
+    setMode(newMode);
+  }
+
+  return { mode, transition };
 }
